7c8a5b9bce9295dec5040ffb35895c40ab022b8c
Risha Uchil <rishauchil278@gmail.com>
2023-12-31 09:00:28 +0530

    first commit

---
+++ emoji-search-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmojiSearch from './EmojiSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Emoji Search App</h1>
+      <EmojiSearch />
+    </div>
+  );
+}
+
+export default App;
